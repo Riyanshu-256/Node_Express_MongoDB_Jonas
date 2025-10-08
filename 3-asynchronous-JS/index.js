61642aacfa84668673840e5289ffa17abb881503
Anshu <riyanshu062@gmail.com>
2025-10-08 22:14:38 +0530

    callback

---
+++ 3-asynchronous-JS/index.js
@@ -0,0 +1,27 @@
+// Importing required modules: 'fs' for file system operations and 'superagent' for making HTTP requests
+const fs = require('fs');
+const superagent = require('superagent');
+
+// Reading the content of 'dog.txt' asynchronously
+fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
+  // Log the breed read from the file
+  console.log(`Breed: ${data}`);
+
+  // Make a GET request to the Dog CEO API to fetch a random image for the breed
+  superagent
+    .get(`https://dog.ceo/api/breed/${data}/images/random`)
+    .end((err, res) => {
+      // Handle any errors from the HTTP request
+      if (err) return console.log(err.message);
+
+      // Log the URL of the random dog image
+      console.log(res.body.message);
+
+      // Save the dog image URL to 'dog-img.txt' asynchronously
+      fs.writeFile('dog-img.txt', res.body.message, err => {
+        if (err) return console.log(err.message);
+        // Confirm that the image URL was successfully saved
+        console.log('Random dog image saved to file!');
+      });
+    });
+});
